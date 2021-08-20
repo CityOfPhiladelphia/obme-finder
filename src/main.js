d81ceaf1f451d61ef289cfbd4af1c70b2a5ea63e
Andy Rothwell <rothwell.andy@gmail.com>
2021-08-20 14:23:28 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,234 @@
+// this is the base-config for resource-finder
+// the point of this file is that it will move outside the project
+// (so that settings we put in it can be used by other projects)
+// and be pulled in with an axios call or something
+// (we might not need to use axios with new vue async tools)
+// if that is not needed, we can move this info to main.js
+
+// turn off console logging in production
+const { hostname='' } = location;
+if (hostname !== 'localhost' && !hostname.match(/(\d+\.){3}\d+/)) {
+  console.log = console.info = console.debug = console.error = function () {};
+}
+
+// import pinboard
+import pinboard from '@phila/pinboard/src/main.js';
+
+import greeting from './general/greeting';
+
+// data-sources
+import immigrant from './data-sources/immigrant';
+// var BASE_CONFIG_URL = 'https://cdn.jsdelivr.net/gh/cityofphiladelphia/mapboard-default-base-config@6126861722cee9384694742363d1661e771493b9/config.js';
+
+import customGreeting from './components/customGreeting.vue';
+const customComps = {
+  // 'expandCollapseContent': expandCollapseContent,
+  'customGreeting': customGreeting,
+};
+
+
+pinboard({
+  // baseConfig: BASE_CONFIG_URL,
+  app: {
+    title: 'Resources for Black males',
+    subtitle: 'Find a resource that helps Black men and boys in Philadelphia ',
+    // logoSrc: require('@/assets/oia-logo.png'),
+    // logoAlt: 'Office of Immigrant Affairs, City of Philadelphia',
+    type: 'immigrant',
+  },
+  gtag: {
+    category: 'rf-oia',
+  },
+  resetDataOnGeocode: true,
+  addressInput: {
+    placeholder: 'Search by address or keyword',
+  },
+  searchBar: {
+    dropdown: [
+      'address',
+      'keyword',
+    ],
+    labelText:  {
+      address: 'Search by address',
+      keyword: 'Search by keyword',
+    },
+    placeholderText: {
+      address: 'Search by address',
+      keyword: 'Search by keyword',
+    },
+  },
+  greeting,
+  locationInfo: {
+    siteName: 'organization_name',
+  },
+  customComps,
+  refine: {
+    type: 'categoryField_array',
+    value: function(item) {
+      return item.services_offered;
+    },
+  },
+  dataSources: {
+    immigrant,
+  },
+  router: {
+    enabled: false,
+  },
+  geocoder: {
+    url(input) {
+      const inputEncoded = encodeURIComponent(input);
+      return `//api.phila.gov/finder/v1/search/${inputEncoded}`;
+    },
+    params: {
+      include_units: true,
+    },
+  },
+  footer: [
+    {
+      type: "native",
+      href: "https://www.phila.gov/",
+      attrs: {
+        target: "_blank",
+      },
+      text: "City Of Philadelphia",
+      // text: "cityOfPhiladelphia",
+    },
+
+    {
+      type: "native",
+      href: "#",
+      text: "About",
+      // text: "about",
+    },
+
+    {
+      type: "native",
+      href: "https://www.phila.gov/feedback/",
+      attrs: {
+        target: "_blank",
+      },
+      text: "Feedback",
+      // text: "feedback",
+    },
+
+    {
+      type: "native",
+      href: 'https://www.phila.gov/departments/mayors-office-of-black-male-engagement/',
+      attrs: {
+        target: "_blank",
+      },
+      text: "Mayor\'s Office of Black Male Engagement",
+    },
+  ],
+  cyclomedia: {
+    enabled: false,
+    // measurementAllowed: false,
+    // popoutAble: true,
+    // recordingsUrl: 'https://atlas.cyclomedia.com/Recordings/wfs',
+    // username: process.env.VUE_APP_CYCLOMEDIA_USERNAME,
+    // password: process.env.VUE_APP_CYCLOMEDIA_PASSWORD,
+    // apiKey: process.env.VUE_APP_CYCLOMEDIA_API_KEY,
+  },
+  markerType: 'pin-marker',
+  map: {
+    type: 'mapbox',
+    // tiles: 'hosted',
+    containerClass: 'map-container',
+    defaultBasemap: 'pwd',
+    center: [ -75.163471, 39.953338 ],
+    zoom: 12,
+    geocodeZoom: 15,
+    imagery: {
+      enabled: false,
+    },
+    basemaps: {
+      pwd: {
+        url: 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer',
+        tiledLayers: [
+          'cityBasemapLabels',
+        ],
+        type: 'featuremap',
+        attribution: 'Parcels: Philadelphia Water',
+      },
+    },
+    tiledLayers: {
+      cityBasemapLabels: {
+        url: 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer',
+        zIndex: '3',
+      },
+    },
+  },
+  mbStyle: {
+    version: 8,
+    sources: {
+      pwd: {
+        tiles: [
+          'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer/tile/{z}/{y}/{x}',
+        ],
+        type: 'raster',
+        tileSize: 256,
+      },
+    },
+    layers: [
+      {
+        id: 'pwd',
+        type: 'raster',
+        source: 'pwd',
+      },
+    ],
+  },
+  basemapSources: {
+    pwd: {
+      source: {
+        tiles: [
+          'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer/tile/{z}/{y}/{x}',
+          // '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer/tile/{z}/{y}/{x}'
+        ],
+        type: 'raster',
+        tileSize: 256,
+      },
+      layer: {
+        id: 'pwd',
+        type: 'raster',
+      },
+    },
+    imagery2019: {
+      source: {
+        tiles: [
+          'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2019_3in/MapServer/tile/{z}/{y}/{x}',
+          // '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer/tile/{z}/{y}/{x}'
+        ],
+        type: 'raster',
+        tileSize: 256,
+      },
+      layer: {
+        id: 'imagery2019',
+        type: 'raster',
+      },
+    },
+  },
+  basemapLabelSources:{
+    cityBasemapLabels: {
+      source: {
+        tiles: [ 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer/tile/{z}/{y}/{x}' ],
+        type: 'raster',
+        tileSize: 256,
+      },
+      layer: {
+        id: 'cityBasemapLabels',
+        type: 'raster',
+      },
+    },
+    imageryBasemapLabels: {
+      source: {
+        tiles: [ 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_Labels/MapServer/tile/{z}/{y}/{x}' ],
+        type: 'raster',
+        tileSize: 256,
+      },
+      layer: {
+        id: 'imageryBasemapLabels',
+        type: 'raster',
+      },
+    },
+  },
+});
