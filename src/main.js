8f52b68532fccc25ef18fee7ba03bd1d946f9cb9
Andy Rothwell <rothwell.andy@gmail.com>
2021-08-23 11:42:58 -0400

    trying push #3

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@
 // (we might not need to use axios with new vue async tools)
 // if that is not needed, we can move this info to main.js
 
-
 // turn off console logging in production
 const { hostname='' } = location;
 if (hostname !== 'localhost' && !hostname.match(/(\d+\.){3}\d+/)) {
