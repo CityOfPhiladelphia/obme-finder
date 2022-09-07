fedad503228802771b94259b5868c5c96a62e80a
Andy Rothwell <Andy.Rothwell@phila.gov>
2022-09-07 12:50:03 -0400

    uses pinboard 1.1.36, cleans up expandCollapseContent file

--- src/components/ExpandCollapseContent.vue
+++ src/components/ExpandCollapseContent.vue
@@ -1,54 +1,87 @@
 <template>
-  <div class="grid-x grid-padding-x">
-    <div class="columns is-marginless">
-      <div
-        v-if="item.attributes.address"
-        class="column columns"
-      >
-        <div class="column is-1">
-          <font-awesome-icon icon="map-marker-alt" />
-        </div>
-        <div class="column is-11">
-          {{ item.attributes.address }}<br>
-          Philadelphia, PA {{ item.attributes.zip_code }}<br>
+  <div :class="isMobile ? 'main-content-mobile' : 'main-content'">
+    <div class="columns">
+      <div class="column is-6">
+        <div
+          v-if="item.attributes.address"
+          class="columns is-mobile"
+        >
+          <div class="column is-1">
+            <font-awesome-icon icon="map-marker-alt" />
+          </div>
+          <div class="column is-11">
+            {{ item.attributes.address }}<br>
+            Philadelphia, PA {{ item.attributes.zip_code }}<br>
+          </div>
         </div>
       </div>
 
-      <div class="column">
+      <div class="column is-6">
         <div
           v-if="item.attributes.phone"
-          class="detail"
+          class="columns is-mobile"
         >
-          <font-awesome-icon icon="phone" />
-          <span>{{ item.attributes.phone }}</span>
+          <div class="column is-1">
+            <font-awesome-icon icon="phone" />
+          </div>
+          <div class="column is-11">
+            {{ item.attributes.phone }}
+          </div>
         </div>
+
         <div
           v-if="item.attributes.email"
-          class="detail"
+          class="columns is-mobile"
         >
-          <font-awesome-icon icon="envelope" />
-          <span><a :href="`mailto:${item.attributes.email}`">{{ item.attributes.email }}</a></span>
+          <div class="column is-1">
+            <font-awesome-icon icon="envelope" />
+          </div>
+          <div class="column is-11">
+            <a :href="`mailto:${item.attributes.email}`">{{ item.attributes.email }}</a>
+          </div>
         </div>
+
         <div
           v-if="item.attributes.website"
-          class="detail"
+          class="columns is-mobile website-div"
         >
-          <font-awesome-icon icon="globe" />
-          <span><a :href="makeValidUrl(item.attributes.website)">{{ item.attributes.website }}</a></span>
+          <div class="column is-1">
+            <font-awesome-icon icon="globe" />
+          </div>
+          <div class="column is-11">
+            <a
+              target="_blank"
+              :href="makeValidUrl(item.attributes.website)"
+            >
+              {{ item.attributes.website }}
+              <font-awesome-icon icon="external-link-alt" />
+            </a>
+          </div>
         </div>
+
         <div
           v-if="item.facebook_name"
-          class="detail"
+          class="columns is-mobile"
         >
-          <font-awesome-icon :icon="['fab', 'facebook']" />
-          <span><a :href="item.facebook_name">Facebook</a></span>
+          <div class="column is-1">
+            <font-awesome-icon :icon="['fab', 'facebook']" />
+          </div>
+          <div class="column is-11">
+            <a :href="item.facebook_name">Facebook</a>
+          </div>
         </div>
+
+
         <div
           v-if="item.twitter"
-          class="detail"
+          class="columns is-mobile"
         >
-          <font-awesome-icon :icon="['fab', 'twitter']" />
-          <span><a :href="item.twitter">Twitter</a></span>
+          <div class="column is-1">
+            <font-awesome-icon :icon="['fab', 'twitter']" />
+          </div>
+          <div class="column is-11">
+            <a :href="item.twitter">Twitter</a>
+          </div>
         </div>
       </div>
     </div>
@@ -67,7 +100,7 @@
     <div
       v-if="item.attributes.tags && item.attributes.tags.length"
     >
-      <h3 class="title section-title is-3 pt-2">
+      <h3 class="title section-title is-3">
         Tags
       </h3>
       <div>
@@ -128,16 +161,16 @@ export default {
 
 <style lang="scss">
 
-.td-style {
-  font-size: 14px !important;
-}
-
-.td-textbox {
-  padding-left: 2rem;
-}
-
-.section-title {
-  margin-bottom: .5rem !important;
-}
+// .td-style {
+//   font-size: 14px !important;
+// }
+//
+// .td-textbox {
+//   padding-left: 2rem;
+// }
+//
+// .section-title {
+//   margin-bottom: .5rem !important;
+// }
 
 </style>
