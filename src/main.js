c6979e0d67feb0d40aac5e5aa447e231176c81e0
Andy Rothwell <Andy.Rothwell@phila.gov>
2023-11-13 15:55:50 -0500

    uses pinboard 1.2.8

--- src/main.js
+++ src/main.js
@@ -155,7 +155,17 @@ pinboard({
     // password: process.env.VUE_APP_CYCLOMEDIA_PASSWORD,
     // apiKey: process.env.VUE_APP_CYCLOMEDIA_API_KEY,
   },
-  markerType: 'pin-marker',
+  markerType: 'circle-marker',
+  circleMarkers: {
+    color: '#9400c6',
+    borderColor: 'white',
+    weight: 1,
+    radius: 8,
+    mobileRadius: 12,
+    size: 16,
+    mobileSize: 20,
+  },
+  // markerType: 'pin-marker',
   map: {
     type: 'mapbox',
     // tiles: 'hosted',
